b4f10075906fe936cae198ce7b8eff1607c34904
long-lb0945 <ly.bao.long@sun-asterisk.com>
2019-10-17 13:35:28 +0700

    edit/destroy category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,5 @@
 module CategoriesHelper
   def cate_sort_opt
-    Category.sort_enums.reduce([]){|a, c| a << [I18n.t((c[0]).to_s), c[0]]}
+    Category.sort_enums.reduce([]){|a, e| a << [I18n.t((e[0]).to_s), e[0]]}
   end
 end
